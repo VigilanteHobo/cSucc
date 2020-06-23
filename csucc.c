f535cd126f3a2278f68d618edd9b1dea7dd47749
VigilanteHobo <60564256+VigilanteHobo@users.noreply.github.com>
2020-06-22 23:34:58 -0400

    Add files via upload

--- csucc.c
+++ csucc.c
@@ -9,26 +9,51 @@ void csucc_readValue(char succFile[32], char valueName[32], void *var) { //csucc
 		return; 
 	}
 	
+	//this needs cleanup.
 	int c;
-	char n = valueName[0];
+	int found = 0;
+	char n = 0;
 	char tempValue[256];
-	while (1==1) {
+	while (1==1) { //check characters in succFile to see if they match valueName.
 		c = fgetc(fp);
-		if (c == n) {
-			
+		if (c == valueName[n]) {
+			found = 1;
+			n++;
 		} else {
-			if (c == ":") {
-				
-				//get value and put it into tempValue, check for line end.
-
+			if (c == ":" &&	found == 1) { //get value and put it into tempValue, check for line end.
+				printf("bryh");
+				int i;
+				fgetc(fp);
+				while(c != "\n" || feof(fp)) {
+					tempValue[i] = c;
+					i++;
+					c = fgetc(fp);
+					printf("%c", c);
+				}
+				break;
+			} else {
+				found = 0;
+				n = 0;
 			}
-			n = valueName[0]
 		}
 
 		if (feof(fp)) { break; }
 	}
+
+	if (found == 1) {
+		//put value into var
+	} else {
+		perror("Value not found");
+		return;
+	}	
 }
 
 void csucc_writeValue(char succFile[32], char valueName[32], void *var) { //csucc_writeValue writes *var to the valueName
 	
+}
+
+int main(void) {
+	int war = 322;
+	csucc_readValue("test.succ", "name", war);
+	return 0;
 }
